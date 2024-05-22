c5454975221829ceb84e3e5ec3f79f6c30629762
Keshavarthan <161265608+Keshavarthan@users.noreply.github.com>
2024-05-22 23:16:00 +1200

    Added my base code, Used functions to create my first window. My font is faulty. I will fix this is in the next commit

---
+++ python.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+
+def main():
+    window = Tk()
+    window.geometry("1024x768")
+    window.title("Quizmania")
+    window.configure(bg="#222831")
+
+    label = Label(window, text="WELCOME TO", font=("Arial", 30), bg="#31363F", fg="#EEEEEE")
+    label.pack(pady=20)
+
+    label = Label(window, text="QUIZPEDIA", font=("Arial", 40))
+    label.pack(pady=20)
+
+    button_frame = Frame(window, bg="#222831")
+    button_frame.pack(pady=20)
+
+    button1 = Button(button_frame, text="Factoids", font=("Comic Sans MS", 25))
+    button1.pack(padx=100, side=LEFT)
+
+    button2 = Button(button_frame, text="Quiz", font=("Comic Sans MS", 25))
+    button2.pack(padx=100, side=LEFT)
+
+    button_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    window.mainloop()
+
+
+main()
