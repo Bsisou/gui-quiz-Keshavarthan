28eb76f7571a6b0933424b434ee8422d2a6ee722
Keshavarthan <161265608+Keshavarthan@users.noreply.github.com>
2024-05-27 18:51:01 +1200

    Changed my font once again for the buttons and slightly increased the width

--- python.py
+++ python.py
@@ -2,6 +2,7 @@ from tkinter import *
 import customtkinter as ctk
 import random
 
+
 def main():
     window = Tk()
     window.geometry("1024x768")
@@ -17,11 +18,12 @@ def main():
     button_frame = Frame(window, bg="#222831")
     button_frame.pack(pady=20)
 
-    button1 = ctk.CTkButton(button_frame, text="Button 1",  width=200, height=100)
-    button1.pack(side="left", padx=10, pady=10)
+    button_font = ctk.CTkFont(family="Helvetica", size=30)  # Change font type and size
+    button1 = ctk.CTkButton(button_frame, text="Quiz", width=200, height=100, fg_color="green", font=button_font)
+    button1.pack(side="left", padx=10)
 
-    button2 = ctk.CTkButton(button_frame, text="Button 2",  width=200, height=100)
-    button2.pack(side="right", padx=10, pady=10)
+    button2 = ctk.CTkButton(button_frame, text="Factoids", width=200, height=100,  fg_color="green", font=button_font)
+    button2.pack(side="right", padx=10)
 
     button_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
