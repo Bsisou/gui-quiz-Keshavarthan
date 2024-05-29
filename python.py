46e4c74fd7ce1c589f971b0fa247a8e6e62e0535
Keshavarthan <161265608+Keshavarthan@users.noreply.github.com>
2024-05-29 13:16:03 +1200

    added button width to make it look cleaner

--- python.py
+++ python.py
@@ -17,10 +17,10 @@ def main():
 
 
     button_font = ctk.CTkFont(family="Helvetica", size=30)
-    button1 = ctk.CTkButton(window, text="Quiz", width=200, height=100, fg_color="#76ABAE", font=button_font, border_width=2)
+    button1 = ctk.CTkButton(window, text="Quiz", width=200, height=100, fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2)
     button1.place(relx=0.3, rely=0.6, anchor=CENTER)
 
-    button2 = ctk.CTkButton(window, text="Factoids", width=200, height=100,  fg_color="#76ABAE", font=button_font, border_width=2)
+    button2 = ctk.CTkButton(window, text="Factoids", width=200, height=100,  fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2)
     button2.place(relx=0.7, rely=0.6, anchor=CENTER)
 
 
