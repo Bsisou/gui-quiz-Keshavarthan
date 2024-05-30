5f3a26bb136aeb399b24838d6cd9c81eb067c1cb
Keshavarthan <161265608+Keshavarthan@users.noreply.github.com>
2024-05-30 21:06:10 +1200

    Revert "Imported time module and re arranged my code"
    This reverts commit be6af3445e8ca0d65f3fa0175fe3524b7f6e6c7c.

--- python.py
+++ python.py
@@ -1,8 +1,8 @@
 from tkinter import *
 import customtkinter as ctk
 from tkinter import PhotoImage
-import time
 import random
+
 
 
 
@@ -17,14 +17,10 @@ def main():
 
 
     button_font = ctk.CTkFont(family="Helvetica", size=30)
-    button1 = ctk.CTkButton(window, text="Quiz", width=200, height=100,
-                            fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2,
-                            hover_color="darkblue")
+    button1 = ctk.CTkButton(window, text="Quiz", width=200, height=100, fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2)
     button1.place(relx=0.3, rely=0.6, anchor=CENTER)
 
-    button2 = ctk.CTkButton(window, text="Factoids", width=200, height=100,
-                            fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2,
-                            hover_color="darkblue")
+    button2 = ctk.CTkButton(window, text="Factoids", width=200, height=100,  fg_color="#76ABAE", font=button_font, border_width=2, corner_radius=2)
     button2.place(relx=0.7, rely=0.6, anchor=CENTER)
 
 
